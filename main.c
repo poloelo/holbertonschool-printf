1d4e28eda2ee7947db9daff5c7a4de3eea87b1ad
poloelo <p.gioria2304@gmail.com>
2025-11-27 10:12:21 +0100

    change the name of the man doc

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int main()
+int main(void)
 {
-    int every_letters = _printf("%");
+    int len;
+
+    len = _printf("Hello %s!\n", "world");
+    _printf("Printed %i characters.\n", len);
+
+    _printf("Show a percent sign: %%\n");
+    _printf("Character: %c\n", 'A');
+    _printf("Integer: %d\n", 123);
 }
